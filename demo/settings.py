ef7b0823af433e83e29b472428844cff101a1f4f
mroscarwilches-debug <oscarwilches.was@gmail.com>
2026-06-17 22:16:48 -0500

    Update requierements dockerization completed

--- demo/settings.py
+++ demo/settings.py
@@ -14,9 +14,9 @@ env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
 
 # Application definition
