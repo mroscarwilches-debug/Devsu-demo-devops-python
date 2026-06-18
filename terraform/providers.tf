a0394e3548948d86e03376a1ae6b1cd1b32126a2
mroscarwilches-debug <oscarwilches.was@gmail.com>
2026-06-18 18:15:53 -0500

    Create terraform files

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+# We're using AWS as our cloud provider.
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
